83871d25b36f417d371e60ecc89231784036cdd7
GenaroHacker <genarominetto@hotmail.com>
2024-06-10 19:49:26 +0200

    leaf creator working

--- scaffolder/builders/facade/modules/branch_file_creator.py
+++ scaffolder/builders/facade/modules/branch_file_creator.py
@@ -1,11 +1,26 @@
-# Branch File Creator Module
+# modules/branch_file_creator.py
 
 from modules.helpers.helper import Helper
 
 class BranchFileCreator:
     def __init__(self, filename):
         self.filename = filename
-        self.helper = Helper()
+        self.helper = Helper(filename)
 
-    def create_branch_file(self):
-        pass
+    def create_branch_file(self, class_name, subcomponents):
+        # Write imports for subcomponents
+        for component in subcomponents:
+            module_path = component.replace('.', '/')
+            self.helper.write_import_line(module_path, component.split('.')[-1].capitalize())
+
+        # Write the class definition
+        self.helper.write_empty_line()
+        self.helper.write_code_line(0, f'class {class_name}:')
+        self.helper.write_code_line(1, 'def __init__(self):')
+
+        # Write the initialization of subcomponents
+        for component in subcomponents:
+            component_name = component.split('.')[-1].lower()
+            self.helper.write_code_line(2, f'self.{component_name} = {component.split(".")[-1].capitalize()}()')
+
+        self.helper.save()
--- scaffolder/builders/facade/modules/leaf_file_creator.py
+++ scaffolder/builders/facade/modules/leaf_file_creator.py
@@ -1,11 +1,24 @@
-# Leaf File Creator Module
+# modules/leaf_file_creator.py
 
 from modules.helpers.helper import Helper
 
 class LeafFileCreator:
     def __init__(self, filename):
         self.filename = filename
-        self.helper = Helper()
+        self.helper = Helper(filename)
 
-    def create_leaf_file(self):
-        pass
+    def create_leaf_file(self, class_name):
+        # Write the class definition
+        self.helper.write_code_line(0, f'class {class_name}:')
+        self.helper.write_code_line(1, 'def __init__(self):')
+        self.helper.write_code_line(1, 'pass')
+        self.helper.write_empty_line()
+        self.helper.write_code_line(1, 'def operation(self):')
+        self.helper.write_code_line(2, f'print("{class_name} operation executed.")')
+
+        self.helper.save()
+
+# Example usage for LeafFileCreator
+if __name__ == "__main__":
+    leaf_creator = LeafFileCreator("chassis.py")
+    leaf_creator.create_leaf_file("Chassis")
