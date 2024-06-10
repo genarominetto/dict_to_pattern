7380fc817abb13674dd8dc5d0e0ddc5c8fce7ca1
GenaroHacker <genarominetto@hotmail.com>
2024-06-10 19:39:28 +0200

    main file creator implemented

--- scaffolder/builders/facade/modules/helpers/helper.py
+++ scaffolder/builders/facade/modules/helpers/helper.py
@@ -1,11 +1,19 @@
 # modules/helpers/helper.py
 
 class Helper:
-    def __init__(self):
-        pass
+    def __init__(self, filepath):
+        self.filepath = filepath
+        self.lines = []
 
     def write_code_line(self, indentation_level, code_line):
-        return ' ' * (indentation_level * 4) + code_line + '\n'
+        self.lines.append(' ' * (indentation_level * 4) + code_line + '\n')
 
     def write_import_line(self, module, class_name):
-        return f'from {module} import {class_name}\n'
+        self.lines.append(f'from {module} import {class_name}\n')
+
+    def write_empty_line(self):
+        self.lines.append('\n')
+
+    def save(self):
+        with open(self.filepath, 'w') as file:
+            file.writelines(self.lines)
--- scaffolder/builders/facade/modules/main_file_creator.py
+++ scaffolder/builders/facade/modules/main_file_creator.py
@@ -1,11 +1,50 @@
-# Main File Creator Module
+# modules/main_file_creator.py
 
 from modules.helpers.helper import Helper
 
 class MainFileCreator:
     def __init__(self, filename):
         self.filename = filename
-        self.helper = Helper()
+        self.helper = Helper(filename)
 
-    def create_main_file(self):
-        pass
+    def create_main_file(self, project_structure):
+        root_class = self._extract_root_class(project_structure)
+        subcomponents = self._extract_subcomponents(project_structure[root_class])
+        
+        self.helper.write_import_line(f"{root_class.lower()}.{root_class.lower()}", root_class)
+        self.helper.write_empty_line()
+        self.helper.write_code_line(0, 'if __name__ == "__main__":')
+        self.helper.write_code_line(1, f'my_{root_class.lower()} = {root_class}()')
+
+        for component in subcomponents:
+            self.helper.write_code_line(1, f'my_{root_class.lower()}.{component}.operation()')
+
+        self.helper.save()
+
+    def _extract_root_class(self, structure):
+        return next(iter(structure))
+
+    def _extract_subcomponents(self, structure, prefix=''):
+        components = []
+        for key, value in structure.items():
+            current_prefix = f"{prefix}.{key.lower()}" if prefix else key.lower()
+            if value:
+                components.extend(self._extract_subcomponents(value, current_prefix))
+            else:
+                components.append(current_prefix)
+        return components
+
+# Example usage
+if __name__ == "__main__":
+    project_structure = {
+        "Car": {
+            "Engine": {
+                "Cylinders": {},
+                "Pistons": {}
+            },
+            "Chassis": {}
+        }
+    }
+    
+    main_creator = MainFileCreator("main.py")
+    main_creator.create_main_file(project_structure)
