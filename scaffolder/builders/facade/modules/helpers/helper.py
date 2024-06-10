e6af306d9a35775e2007d754bef874d564fc9a3e
GenaroHacker <genarominetto@hotmail.com>
2024-06-10 19:31:53 +0200

    add helper method to the helper class

--- scaffolder/builders/facade/modules/helpers/helper.py
+++ scaffolder/builders/facade/modules/helpers/helper.py
@@ -1,8 +1,11 @@
-# Helper Module
+# modules/helpers/helper.py
 
 class Helper:
     def __init__(self):
         pass
 
-    def assist(self):
-        pass
+    def write_code_line(self, indentation_level, code_line):
+        return ' ' * (indentation_level * 4) + code_line + '\n'
+
+    def write_import_line(self, module, class_name):
+        return f'from {module} import {class_name}\n'
--- scaffolder/builders/facade/project_creator.py
+++ scaffolder/builders/facade/project_creator.py
@@ -1,4 +1,4 @@
-# Project Creator Module
+# project_creator.py
 
 from modules.main_file_creator import MainFileCreator
 from modules.branch_file_creator import BranchFileCreator
@@ -7,8 +7,23 @@ from modules.test_file_creator import TestFileCreator
 from modules.simple_file_creator import SimpleFileCreator
 
 class ProjectCreator:
-    def __init__(self, project_name):
+    def __init__(self, project_name, project_structure):
         self.project_name = project_name
+        self.project_structure = project_structure
 
     def create_project(self):
         pass
+
+if __name__ == "__main__":
+    project_structure = {
+        "Car": {
+            "Engine": {
+                "Cylinders": {},
+                "Pistons": {}
+            },
+            "Chassis": {}
+        }
+    }
+
+    creator = ProjectCreator("FacadeProject", project_structure)
+    creator.create_project()
