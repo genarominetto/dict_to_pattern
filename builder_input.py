4b7a3c02052aa4a8404440c40175a05373790f96
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2024-06-23 21:09:30 +0200

    Create builder_input.py

---
+++ builder_input.py
@@ -0,0 +1,6 @@
+project_structure = {
+    "product": "house",
+    "types": ["modern", "victorian"],
+    "abstract_parts": ["foundation", "structure"],
+    "concrete_parts": ["roof", "interior"]
+}
