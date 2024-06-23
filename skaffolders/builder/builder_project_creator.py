c2bb079f5f0e4195b01350db79d1b9f63acec8b7
GenaroHacker <genarominetto@hotmail.com>
2024-06-23 23:52:27 +0200

    add import lines

--- skaffolders/builder/builder_project_creator.py
+++ skaffolders/builder/builder_project_creator.py
@@ -1,13 +1,20 @@
+from modules.abstract_builder_file_creator import AbstractBuilderFileCreator
+from modules.concrete_builder_file_creator import ConcreteBuilderFileCreator
+from modules.director_file_creator import DirectorFileCreator
+from modules.main_file_creator import MainFileCreator
+from modules.product_file_creator import ProductFileCreator
+from modules.simple_file_creator import SimpleFileCreator
+from modules.test_file_creator import TestFileCreator
 import os
 import shutil
 
 class StructureHelper:
-    def __init__(self, project_structure):
+    def __init__(self, project_structure, root_module):
         self.project_structure = project_structure
         self.root_module = root_module
 
 class BuilderProjectCreator:
-    def __init__(self, project_name, project_structure):
+    def __init__(self, project_name, project_structure, root_module):
         self.project_name = project_name
         self.project_structure = project_structure
         self.root_module = root_module
