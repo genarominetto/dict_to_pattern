d206cdb6904309ed711c2a84198b6c92e1c177c3
GenaroHacker <95663273+GenaroHacker@users.noreply.github.com>
2024-06-24 00:48:22 +0200

    Update builder_input.py

--- temp_target/builder_input.py
+++ temp_target/builder_input.py
@@ -4,3 +4,30 @@ project_structure = {
     "abstract_parts": ["FoundationBase", "StructureBase"],
     "concrete_parts": ["InteriorDesign", "OutdoorDesign"]
 }
+
+
+
+
+project/
+│
+├── builders/
+│   ├── abstract_builder/
+│   │   └── vacation_house_builder.py
+│   ├── ocean_beach_vacation_house_builder.py
+│   └── rock_mountain_vacation_house_builder.py
+│
+├── product/
+│   └── vacation_house.py
+│
+├── parts/
+│   ├── foundation_base.py
+│   ├── structure_base.py
+│   ├── interior_design.py
+│   └── outdoor_design.py
+│
+├── director/
+│   └── director.py
+│
+└── tests/
+    ├── test_ocean_beach_vacation_house.py
+    └── test_rock_mountain_vacation_house.py
