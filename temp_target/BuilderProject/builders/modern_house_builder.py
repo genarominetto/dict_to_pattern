4c969fb71a19acc71f6bda9aa3192ac294eee73f
GenaroHacker <genarominetto@hotmail.com>
2024-08-21 13:03:01 +0200

    replace builder target

--- temp_target/BuilderProject/builders/modern_house_builder.py
+++
@@ -1,8 +0,0 @@
-from builders.abstract_builder.house_builder import HouseBuilder
-
-class ModernHouseBuilder(HouseBuilder):
-    def build_roof(self):
-        self.house.roof = "Modern Roof"
-
-    def build_interior(self):
-        self.house.interior = "Modern Interior"
--- temp_target/BuilderProject/builders/victorian_house_builder.py
+++
@@ -1,8 +0,0 @@
-from builders.abstract_builder.house_builder import HouseBuilder
-
-class VictorianHouseBuilder(HouseBuilder):
-    def build_roof(self):
-        self.house.roof = "Victorian Roof"
-
-    def build_interior(self):
-        self.house.interior = "Victorian Interior"
