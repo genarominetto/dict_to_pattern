5d37836dbcb17123a96807c36fe494b758359d2c
genarominetto <95663273+genarominetto@users.noreply.github.com>
2024-08-31 19:21:37 +0200

    Update and rename builder_input.py to state_input.py

---
+++ temp_target/state_input.py
@@ -0,0 +1,21 @@
+project_structure = {
+}
+
+
+
+
+.
+├── light_switch
+│   ├── light_switch.py
+│   ├── states
+│   │   ├── abstract
+│   │   │   └── state.py
+│   │   ├── off_state.py
+│   │   └── on_state.py
+│   └── tests
+│       ├── test_off_state.py
+│       └── test_on_state.py
+├── main.py
+└── pytest.ini
+
+
