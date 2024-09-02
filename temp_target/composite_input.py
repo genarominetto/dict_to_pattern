1a099354a8be578e7e5450b23ded0c0f74b55e2d
genarominetto <95663273+genarominetto@users.noreply.github.com>
2024-09-02 23:25:00 +0200

    Update and rename state_input.py to composite_input.py

---
+++ temp_target/composite_input.py
@@ -0,0 +1,35 @@
+project_structure = {
+    "component" : "graphic",
+    "composite" : "group",
+    "leaves" : ["circle", "square"],
+    "leaf_properties" : {
+        "str" : ["name"],
+        "int" : ["x", "y"],
+        "float" : ["size"],
+        "bool" : ["active"]
+    }
+}
+
+
+
+
+
+.
+├── graphic_drawing
+│   ├── base
+│   │   └── graphic.py
+│   ├── components
+│   │   ├── composite
+│   │   │   └── group.py
+│   │   └── leafs
+│   │       ├── abstract
+│   │       │   └── leaf.py
+│   │       ├── circle.py
+│   │       └── square.py
+│   └── tests
+│       ├── test_circle.py
+│       └── test_square.py
+├── main.py
+
+
+
