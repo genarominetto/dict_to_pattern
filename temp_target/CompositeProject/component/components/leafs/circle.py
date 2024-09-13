e877075c18fb2193a8c176f4d2c81497d3783d91
GenaroHacker <genarominetto@hotmail.com>
2024-09-13 14:03:19 +0200

    reset the composite scaffolder modules

--- temp_target/CompositeProject/component/components/leafs/circle.py
+++ temp_target/CompositeProject/component/components/leafs/circle.py
@@ -2,5 +2,5 @@ from component.components.leafs.abstract.leaf import Leaf
 
 # Concrete class for a circle
 class Circle(Leaf):
-    def __init__(self, *, name: str, size: float, is_active: bool = True):
+    def __init__(self, *, name: str, size: float, is_active: bool):
         super().__init__(name=name, size=size, is_active=is_active)
