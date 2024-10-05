10e5c8156a4f0bd7c4b0897e18ab8f742984384b
GenaroHacker <genarominetto@hotmail.com>
2024-10-05 19:01:25 +0200

    add init files

--- temp_target/CompositeProject/component/components/leafs/circle.py
+++
@@ -1,6 +0,0 @@
-from component.components.leafs.abstract.leaf import Leaf
-
-# Concrete class for a circle
-class Circle(Leaf):
-    def __init__(self, *, name: str, size: float, is_active: bool):
-        super().__init__(name=name, size=size, is_active=is_active)
